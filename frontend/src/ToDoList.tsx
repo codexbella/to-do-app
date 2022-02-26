e202e44d8f0888ffe75cd5b4a88f4a132cdb2044
Anna Lena Baumann <rescht@posteo.de>
2022-02-26 16:58:41 +0100

    search and additem implemented

--- frontend/src/ToDoList.tsx
+++ frontend/src/ToDoList.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React, {ReactEventHandler, useEffect, useState} from "react";
 import {ToDoItem, ResponseBody} from "./itemModel";
 import ToDoGalleryItem from "./ToDoGalleryItem";
 
@@ -7,32 +7,73 @@ export default function ToDoList() {
     const [searchTerm, setSearchTerm] = useState('');
     const [urlEnd, setUrlEnd] = useState('getall');
     const [toDoList, setToDoList] = useState([] as Array<ToDoItem>);
+    const [currentItem, setCurrentItem] = useState({title: 'test', description: '', done: false} as ToDoItem);
+    const [askingMethod, setAskingMethod] = useState('GET');
 
-    const fetchData = (url: string = 'http://localhost:8080/todoitems/'+urlEnd) => {
-        fetch(url)
-            .then(response => response.json())
-            .then((response: Array<ToDoItem>) => {
-                setToDoList(response)
-            }).catch(() => console.log('oopsie'))
+    const askBackend = (url: string = urlEnd) => {
+        if (askingMethod === 'GET') {
+            fetch('http://localhost:8080/todoitems/'+url)
+                .then(response => response.json())
+                .then((list: Array<ToDoItem>) => {
+                    setToDoList(list)
+                }).catch(() => console.log('oopsie - get'))
+        } else if (askingMethod === 'POST') {
+            fetch('http://localhost:8080/todoitems/'+url, {
+                method: 'POST',
+                body: JSON.stringify(currentItem),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((list: Array<ToDoItem>) => {
+                    setToDoList(list)
+                }).catch(() => console.log('oopsie - post'))
+        }
     }
 
-    useEffect(() => fetchData('http://localhost:8080/todoitems/'+urlEnd), [urlEnd])
+    useEffect(() => askBackend(urlEnd), [urlEnd])
+
+    const handleSearchInput = (input: string) => {
+        setAskingMethod('GET')
+        if (input === '') {
+            setUrlEnd('getall')
+        } else {
+            setUrlEnd(input)
+        }
+        setSearchTerm(input)
+    }
+    const changeCurrentItemTitle = (input: string) => {
+        setCurrentItem({
+            title: input,
+            description: currentItem.description,
+            done: currentItem.done})
+    }
+    const changeCurrentItemDescription = (input: string) => {
+        setCurrentItem({
+            title: currentItem.title,
+            description: input,
+            done: currentItem.done})
+    }
 
     return <div><h1>To-Do-Liste</h1>
-        <button onClick={() => setUrlEnd('getall')} className='getall-button'>Alle anzeigen</button>
-        <button onClick={() => setUrlEnd('getallnotdone')} className='getallnotdone-button'>Offene anzeigen</button>
-        <button className='additem-button'>Neues To-Do anlegen</button>
-
+        <button onClick={() => {setUrlEnd('getall'); setAskingMethod('GET'); setSearchTerm('')}} className='getall-button'>Alle anzeigen</button>
+        <button onClick={() => {setUrlEnd('getallnotdone'); setAskingMethod('GET'); setSearchTerm('')}} className='getallnotdone-button'>Offene anzeigen</button>
         <div>
-            <h3>To-Do(s) filtern:</h3>
-            <input type='text' placeholder='Tippe deinen Suchbegriff ein.' value={searchTerm}
-                   onChange={typed => setSearchTerm(typed.target.value)} className='search-field'/>
+            <input type='text' placeholder='Titel' onChange={typed => changeCurrentItemTitle(typed.target.value)} className='new-to-do-item-title'/>
+            <input type='text' placeholder='Beschreibung' onChange={typed => changeCurrentItemDescription(typed.target.value)} className='new-to-do-item-description'/>
+            <button onClick={() => {setUrlEnd('additem'); setAskingMethod('POST'); setSearchTerm('')}} className='additem-button'>Neues To-Do anlegen</button>
         </div>
 
         <div>
-            {toDoList.map(item => <ToDoGalleryItem toDoItem={item}/>)}
+            <h3>Liste filtern:</h3>
+            <input type='text' placeholder='Suchbegriff' value={searchTerm}
+                   onChange={typed => handleSearchInput(typed.target.value)} className='search-field'/>
         </div>
 
-        <div>searchItem</div>
+        <div>
+            {toDoList.map(item => <ToDoGalleryItem toDoItem={item} key={JSON.stringify(item)}/>)}
+        </div>
+
     </div>
 }
