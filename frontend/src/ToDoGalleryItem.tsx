b586db8f149aef8dab07bc25a1cb8f84410eefd8
Anna Lena Baumann <rescht@posteo.de>
2022-02-28 15:05:13 +0100

    useEffect solved, refactoring and delete and change status implemented

--- frontend/src/ToDoGalleryItem.tsx
+++ frontend/src/ToDoGalleryItem.tsx
@@ -1,23 +1,58 @@
-import {ToDoItem} from "./itemModel";
+import {ToDoItem} from './itemModel';
+import './ToDoGalleryItem.css';
+import pen from './images/edit-pen.png';
 
-interface ToDoItemProps {
+interface ToDoGalleryItemProps {
     toDoItem: ToDoItem;
+    onChange: (list: Array<ToDoItem>) => void;
 }
 
-const determineStatus = (input: boolean) => {
-    if (!input) {
-        return "offen"
-    } else {
-        return "erledigt"
+export default function ToDoGalleryItem(props: ToDoGalleryItemProps) {
+    const deleteToDo = (itemToDelete: ToDoItem) => {
+        fetch('http://localhost:8080/todoitems/delete', {
+            method: 'POST',
+            body: JSON.stringify(itemToDelete),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then((list: Array<ToDoItem>) => props.onChange(list))
+            .catch(() => console.log('oopsie - deleteItem'))
     }
-}
+    const changeStatus = (itemToChange: ToDoItem) => {
+        if (itemToChange.done) {
+            fetch('http://localhost:8080/todoitems/setasnotdone', {
+                method: 'POST',
+                body: JSON.stringify(itemToChange),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((list: Array<ToDoItem>) => props.onChange(list))
+                .catch(() => console.log('oopsie - changeItem'))
+        } else {
+            fetch('http://localhost:8080/todoitems/setasdone', {
+                method: 'POST',
+                body: JSON.stringify(itemToChange),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((list: Array<ToDoItem>) => props.onChange(list))
+                .catch(() => console.log('oopsie - changeItem'))
+        }
+    }
 
-export default function ToDoGalleryItem(props: ToDoItemProps) {
     return (
-        <div className="to-do-item">
-            <h3 className="to-do-item-title">{ props.toDoItem.title }</h3>
-            <p className="gallery-item-description">{ props.toDoItem.description }</p>
-            <p className="gallery-item-done">{ determineStatus(props.toDoItem.done) }</p>
-        </div>
+        <span className={props.toDoItem.done ? 'to-do-item-done': 'to-do-item-not-done'}>
+            <div className="to-do-item-title">{ props.toDoItem.title }</div>
+            <span className="gallery-item-description">{ props.toDoItem.description }</span>
+            <img className="item-edit-pen" src={pen} alt='Bearbeiten'/>
+            <span id="checkmark" onClick={() => changeStatus(props.toDoItem)}>&#10004;</span>
+            <span id="x-for-delete" onClick={() => deleteToDo(props.toDoItem)} >&#10005;</span>
+        </span>
     )
 }
--- frontend/src/ToDoList.tsx
+++ frontend/src/ToDoList.tsx
@@ -1,95 +1,103 @@
 import React, {useEffect, useState} from "react";
 import {ToDoItem} from "./itemModel";
 import ToDoGalleryItem from "./ToDoGalleryItem";
+import './ToDoList.css';
 
 export default function ToDoList() {
     const [searchTerm, setSearchTerm] = useState('');
-    const [urlEnd, setUrlEnd] = useState('getall');
     const [toDoList, setToDoList] = useState([] as Array<ToDoItem>);
     const [currentItem, setCurrentItem] = useState({title: 'test', description: '', done: false} as ToDoItem);
-    const [askingMethod, setAskingMethod] = useState('GET');
+    const [titleField, setTitleField] = useState('');
+    const [descriptionField, setDescriptionField] = useState('');
 
-    const askBackend = (url: string = urlEnd) => {
-        if (askingMethod === 'GET') {
-            fetch('http://localhost:8080/todoitems/'+url)
-                .then(response => response.json())
-                .then((list: Array<ToDoItem>) => {
-                    setToDoList(list)
-                }).catch(() => console.log('oopsie - get'))
-        } else if (askingMethod === 'POST') {
-            fetch('http://localhost:8080/todoitems/'+url, {
-                method: 'POST',
-                body: JSON.stringify(currentItem),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => response.json())
-                .then((list: Array<ToDoItem>) => {
-                    setToDoList(list)
-                }).catch(() => console.log('oopsie - post'))
-        }
+    const getAll = () => {
+        fetch('http://localhost:8080/todoitems/getall')
+            .then(response => response.json())
+            .then((list: Array<ToDoItem>) => {
+                setToDoList(list)
+            }).catch(() => console.log('oopsie - getAll'))
+    }
+    const getAllNotDone = () => {
+        fetch('http://localhost:8080/todoitems/getallnotdone')
+            .then(response => response.json())
+            .then((list: Array<ToDoItem>) => {
+                setToDoList(list)
+            }).catch(() => console.log('oopsie - getAllNotDone'))
     }
 
-    useEffect(() => askBackend('getall'), [])
+    const addItem = (item: ToDoItem) => {
+        fetch('http://localhost:8080/todoitems/additem', {
+            method: 'POST',
+            body: JSON.stringify(item),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then((list: Array<ToDoItem>) => {
+                setToDoList(list)
+            }).catch(() => console.log('oopsie - addItem'))
+    }
 
-    const handleSearchInput = (input: string) => {
-        setAskingMethod('GET')
+    useEffect(() => getAll(), [])
+
+    const getMatchingItems = (input: string) => {
         if (input === '') {
-            setUrlEnd('getall')
-            askBackend('getall')
+            getAll()
         } else {
-            setUrlEnd(input)
-            askBackend(input)
+            fetch('http://localhost:8080/todoitems/'+input)
+                .then(response => response.json())
+                .then((list: Array<ToDoItem>) => {
+                    setToDoList(list)
+                }).catch(() => console.log('oopsie - search'))
         }
         setSearchTerm(input)
     }
+
     const changeCurrentItemTitle = (input: string) => {
         setCurrentItem({
             title: input,
             description: currentItem.description,
             done: currentItem.done})
+        setTitleField(input)
     }
     const changeCurrentItemDescription = (input: string) => {
         setCurrentItem({
             title: currentItem.title,
             description: input,
             done: currentItem.done})
+        setDescriptionField(input)
     }
+
 
     return <div><h1 id='title-to-do-list'>To-Do-Liste</h1>
 
-        <button onClick={() => {
-            setAskingMethod('GET')
-            setSearchTerm('')
-            askBackend('getall')
-        }
-        } className='getall-button'>Alle anzeigen</button>
-
-        <button onClick={() => {
-            setAskingMethod('GET')
-            setSearchTerm('')
-            askBackend('getallnotdone')
-        }
-        } className='getallnotdone-button'>Offene anzeigen</button>
+        <button className='getall-button' onClick={getAll}>Alle anzeigen</button>
+        <button className='getallnotdone-button' onClick={getAllNotDone}>Offene anzeigen</button>
 
         <div>
-            <input type='text' placeholder='Titel' onChange={typed => changeCurrentItemTitle(typed.target.value)} className='new-to-do-item-title'/>
-            <input type='text' placeholder='Beschreibung' onChange={typed => changeCurrentItemDescription(typed.target.value)} className='new-to-do-item-description'/>
-            <button onClick={() => {
-                setAskingMethod('POST')
-                setSearchTerm('')
-                askBackend('additem')}} className='additem-button'>Neues To-Do anlegen</button>
+            <input className='new-to-do-item-title' type='text' placeholder='Titel' value={titleField}
+                   onChange={typed => changeCurrentItemTitle(typed.target.value)}
+            />
+
+            <input className='new-to-do-item-description' type='text' placeholder='Beschreibung' value={descriptionField}
+                   onChange={typed => changeCurrentItemDescription(typed.target.value)}
+            />
+
+            <button className='additem-button' onClick={() => {addItem(currentItem); setTitleField(''); setDescriptionField('')}}>
+                Neues To-Do anlegen
+            </button>
+
         </div>
 
         <div>
             <h3>Liste filtern:</h3>
-            <input type='text' placeholder='Suchbegriff' value={searchTerm}
-                   onChange={typed => handleSearchInput(typed.target.value)} className='search-field'/>
+            <input className='search-field' type='text' placeholder='Suchbegriff' value={searchTerm}
+                   onChange={typed => getMatchingItems(typed.target.value)}/>
         </div>
 
-        <div>
-            {toDoList.map(item => <ToDoGalleryItem toDoItem={item} key={JSON.stringify(item.title)}/>)}
+        <div id="to-do-items-wrapper">
+            {toDoList.map(item => <ToDoGalleryItem toDoItem={item} key={JSON.stringify(item.title)} onChange={setToDoList}/>)}
         </div>
     </div>
 }
