03882ccb7cc94fc2e3004147d6f2320005f3e56b
Anna Lena Baumann <rescht@posteo.de>
2022-03-17 18:24:42 +0100

    SecurityConfig und UserController added

---
+++ backend/src/main/java/de/codexbella/SecurityConfig.java
@@ -0,0 +1,37 @@
+package de.codexbella;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+   private final UserDetailsService userDetailsService;
+
+   @Override
+   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(userDetailsService);
+   }
+
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+      http.csrf().disable()
+            .authorizeRequests()
+            .mvcMatchers("/api/todoitems").authenticated()
+            .mvcMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+            .antMatchers("/**").authenticated();
+   }
+
+   @Bean
+   public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+   }
+}
---
+++ backend/src/main/java/de/codexbella/user/UserController.java
@@ -0,0 +1,22 @@
+package de.codexbella.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+   private final UserService userService;
+   private final PasswordEncoder passwordEncoder;
+
+   @PostMapping("/register")
+   public String createUser(@RequestBody UserDocument user) {
+      user.setPassword(passwordEncoder.encode(user.getPassword()));
+      return userService.createUser(user);
+   }
+}
