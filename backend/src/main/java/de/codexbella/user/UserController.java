8e6e41673d944dab1d5407a590abb128950fddbb
Anna Lena Baumann <rescht@posteo.de>
2022-04-02 20:02:29 +0200

    removed unused imports in UserController

--- backend/src/main/java/de/codexbella/user/UserController.java
+++ backend/src/main/java/de/codexbella/user/UserController.java
@@ -11,10 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Map;
 
